211b1b0925e9390d87b61840663cae6c981e1fb6
dinhtho2001 <dinhtho357@gmail.com>
2022-07-08 08:50:43 +0700

    update

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,64 +1,63 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.dto.EmployeeDTO;
 import com.example.demo.dto.response.EmployeeResponse;
 import com.example.demo.service.IEmployeeService;
 
-@CrossOrigin
 @RestController
+@RequestMapping("/employees")
 public class EmployeeController {
 
 	@Autowired
 	private IEmployeeService service;
 	
-	@GetMapping(value="/employees")
+	@GetMapping
 	public EmployeeResponse all(@RequestParam("page") int page, @RequestParam("limit") int limit) {
 		
 		return service.findAll(page, limit);
 	}
 	
-	@GetMapping(value="/employee/{id}")
+	@GetMapping(value="/{id}")
 	public EmployeeDTO one(@PathVariable("id") Long id) {
 		
 		return service.findOne(id);
 	}
 	
-	@PostMapping(value="/employees")
+	@PostMapping
 	public EmployeeDTO[] creates(@RequestBody EmployeeDTO[] dtos ) {
 		
 		return service.creates(dtos);
 	}
-	@PostMapping(value="/employee")
+	@PostMapping
 	public EmployeeDTO create(@RequestBody EmployeeDTO dto ) {
 		
 		return service.create(dto);
 	}
 	
-	@PutMapping(value="/employee/{id}")
+	@PutMapping(value="/{id}")
 	public EmployeeDTO update(@RequestBody EmployeeDTO dto, @PathVariable("id") Long id) {
 		dto.setEmpId(id);
 		return service.update(dto);
 	}
 	
-	@DeleteMapping(value="/employee/{id}")
+	@DeleteMapping(value="/{id}")
 	public String delete(@PathVariable("id") Long id) {
 		
 		return service.delete(id);
 	}
 	
-	@DeleteMapping(value="/employees")
+	@DeleteMapping
 	public String deletes(@RequestBody Long[] ids) {
 		
 		return service.deletes(ids);
